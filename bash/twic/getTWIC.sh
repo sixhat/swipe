0bb878dd08b388aaadb87bfd7ecd47ae5c7b3867
David.Rodrigues <david@sixhat.net>
2014-02-24 18:01:25 +0000

    This Week In Chess Builds a Big Database

---
+++ bash/twic/getTWIC.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Code inspired and adapted from the code of twic-db-aggregator 
+# http://code.google.com/p/twic-db-aggregator/
+# License GPL v2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+I=$1 
+F=$2
+while [ $I -lt $2 ]; do
+    wget http://www.theweekinchess.com/zips/twic${I}g.zip
+  if [ -f twic${I}g.zip ]; then
+    unzip twic${I}g.zip
+    cat twic${I}.pgn >> big.pgn
+  fi
+  I=`expr $I + 1`
+done
+
+rm -rf twic*
